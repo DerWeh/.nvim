a6598aea89faba2ffee434db49a90c28c4aed2c9
DerWeh <andreas.weh@web.de>
2022-10-26 23:24:51 +0200

    Highlight yanks

--- init.lua
+++ init.lua
@@ -244,3 +244,11 @@ require('gitsigns').setup({
     map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 })
+
+-- autocmds
+vim.api.nvim_create_augroup("highlight_yank", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = "highlight_yank",
+  callback = function() vim.highlight.on_yank { higroup = "IncSearch", timeout = 700 } end
+})
+
