e1ff2cf11ce77cc462f3edfa9e8722906186fed9
DerWeh <andreas.weh@web.de>
2022-11-08 19:34:45 +0100

    Drop printing of language servers on startup
    Statement is executed independent of starting of language servers,
    which is anoying. Should be moved to `on_attach` instead.
    However, I don't know how to access the name of the langugage server.

--- init.lua
+++ init.lua
@@ -114,6 +114,7 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
+  print('Starting language server')
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local wk = require("which-key")
@@ -149,7 +150,6 @@ local on_attach = function(_, bufnr)
 end
 require('mason-lspconfig').setup_handlers({
   function(server)
-    print('Starting language server', server)
     if server == "sumneko_lua" then
       lspconfig[server].setup({ on_attach = on_attach, settings = { Lua = { diagnostics = { globals = { 'vim' } } } } })
     else
