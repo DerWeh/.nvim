e4413a12cefd9d59b49115b385e2beedbc9160d1
DerWeh <andreas.weh@web.de>
2022-12-06 21:41:11 +0100

    Add autocompletion

--- init.lua
+++ init.lua
@@ -83,6 +83,19 @@ require('packer').startup(function(use)
     -- end
   })
 
+  -- auto completion
+  use({
+    'hrsh7th/cmp-nvim-lsp',
+    'ray-x/lsp_signature.nvim',
+    -- 'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+  })
+  use({
+    'hrsh7th/nvim-cmp',
+    tag = "*",
+  })
+
   use({
     'nvim-telescope/telescope.nvim',
     tag = '*',
@@ -96,6 +109,49 @@ require('packer').startup(function(use)
   end
 end)
 -- END: packer
+
+-- setup cmp
+-- Set up nvim-cmp.
+local cmp = require('cmp')
+cmp.setup({
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'nvim_lsp_signature_help' },
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+require("lsp_signature").setup()
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 
 -- language server configuration
