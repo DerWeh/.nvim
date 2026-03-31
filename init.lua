3814138725914577129100b4bdaf8e2b30bbee04
DerWeh <andreas.weh@web.de>
2026-03-31 14:07:12 +0200

    BUG: drop overwritten mapping

--- init.lua
+++ init.lua
@@ -117,7 +117,6 @@ require("lazy").setup({
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
